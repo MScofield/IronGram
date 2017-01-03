22c6302a24cdfd8b58cc5324dd1b86e1f4006c83
Matthew Scofield <matthewgscofield@outlook.com>
2017-01-03 15:20:17 -0600

    lesson portion functioning

---
+++ src/main/java/com/ironyard/services/PhotoRepository.java
@@ -0,0 +1,14 @@
+package com.ironyard.services;
+
+import com.ironyard.entities.Photo;
+import com.ironyard.entities.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+/**
+ * Created by scofieldservices on 1/3/17.
+ */
+public interface PhotoRepository extends CrudRepository<Photo, Integer> {
+    List<Photo> findByRecipient(User receiver);
+}
